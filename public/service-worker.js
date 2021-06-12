0133aa8c7212427b5c17af158af67e4c438a81f4
Matthew Goad <matthewxgoad@gmail.com>
2021-06-11 20:12:57 -0500

    updated service-worker install code

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,16 +13,10 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
-    
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-
   // tell the browser to activate this service worker immediately once it
   // has finished installing
   self.skipWaiting();
